e01af022488c23746cbf42286be670304e1eb0d8
inxeoz <inxeoz@inxeoz.com>
2025-12-18 17:53:18 +0530

    no cache

--- run.sh
+++ run.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
 if [ $# -lt 2 ]; then
-  echo "Usage: $0 <apps_json_file> <apps_to_install> [port]"
-  echo "Example: $0 apps_mahakaal.json 'mahakaal' 8890"
+  echo "Usage: $0 <apps_json_file> <apps_to_install> [port] [--no-cache]"
+  echo "Example: $0 apps_mahakaal.json 'mahakaal' 8890 --no-cache"
   exit 1
 fi
 
 APPS_JSON_FILE=$1
 APPS_TO_INSTALL=$2
 FRONTEND_PORT=${3:-8080}
+NO_CACHE=""
+if [ "$4" = "--no-cache" ]; then
+  NO_CACHE="--no-cache"
+fi
 
 if [ ! -f "$APPS_JSON_FILE" ]; then
   echo "Apps JSON file not found: $APPS_JSON_FILE"
@@ -25,7 +29,7 @@ echo "############# APPS_JSON_BASE64 ################"
 echo $APPS_JSON_BASE64
 echo "############# APPS_JSON_BASE64 ################"
 
-docker compose -f pwd.yml build
+docker compose -f pwd.yml build $NO_CACHE
 
 docker compose -f pwd.yml up -d
 
